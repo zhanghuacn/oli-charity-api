2f636d100df6ac1071e3c68350f0b72e25d688ec
zhanghua <zhangkaneki@gmail.com>
2021-12-23 21:06:24 +0800

    fix bug
    Signed-off-by: zhanghua <zhangkaneki@gmail.com>

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -40,7 +40,6 @@ class RouteServiceProvider extends ServiceProvider
         $this->routes(function () {
             Route::prefix('api/v1')
                 ->middleware('api')
-                ->namespace($this->namespace)
                 ->group(base_path('routes/api.php'));
 
             Route::prefix('charity/v1')
