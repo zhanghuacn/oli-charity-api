187c62bd3656b5f8b46705730b2346059228ba73
Zhang Hua <zhanghua_hui@qq.com>
2022-02-18 00:31:03 +0800

    fix
    Signed-off-by: Zhang Hua <zhanghua_hui@qq.com>

--- app/Http/Controllers/Charity/V1/LotteryController.php
+++ app/Http/Controllers/Charity/V1/LotteryController.php
@@ -22,16 +22,17 @@ class LotteryController extends Controller
 {
     public function draw(Lottery $lottery): JsonResponse|JsonResource
     {
-        abort_if($lottery->status, 422, 'Please do not repeat the lottery');
+//        abort_if($lottery->status, 422, 'Please do not repeat the lottery');
         DB::transaction(function () use ($lottery) {
-            $tickets = $lottery->activity->tickets()->where('amount', '>=', $lottery->standard_amount)
-                ->pluck('amount', 'user_id')->toArray();
-            Log::info('tickets:', ['ticket' => $tickets]);
+            $result = $lottery->activity->tickets()->where('amount', '>=', $lottery->standard_amount);
+            abort_if($result->doesntExist(), 500, 'Too few participants in the lottery');
+            $tickets = $result->pluck('amount', 'user_id')->toArray();
+            Log::info('tickets:' . json_encode($tickets));
             $money = collect($tickets)->values();
             $ids = array_keys($tickets);
             $n = min($lottery->prizes()->sum('num'), count(array_keys($tickets)));
             $data = ['money' => $money, 'ids' => $ids, 'n' => $n];
-            Log::info('data:', ['data' => $data]);
+            Log::info('data:' . json_encode($data));
             abort_if(empty($money) || empty($ids) || empty($n), 500, 'Abnormal lottery conditions');
             $body = Http::post(config('services.custom.lottery_url'), $data)->body();
             $result = json_decode($body, true);
