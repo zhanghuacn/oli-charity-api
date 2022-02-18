acd463d3a9872618783a070a056b90c63c8d501f
Zhang Hua <zhanghua_hui@qq.com>
2022-02-18 23:50:47 +0800

    fix
    Signed-off-by: Zhang Hua <zhanghua_hui@qq.com>

---
+++ app/Http/Controllers/Admin/V1/HomeController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Admin\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\Activity;
+use App\Models\Charity;
+use App\Models\Sponsor;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\DB;
+use Jiannei\Response\Laravel\Support\Facades\Response;
+
+class HomeController extends Controller
+{
+    public function dashboard(): JsonResponse|JsonResource
+    {
+        $result = DB::table('users')->whereBetween('created_at', [now()->addDays(-7)->startOfDay(), now()->endOfDay()])
+            ->selectRaw('date(created_at) as date,count(*) as num')->groupBy('date')->pluck('num', 'date')->toArray();
+        for ($i = 7; $i >= 1; $i--) {
+            if (!array_key_exists(now()->subDays($i)->toDateString(), $result)) {
+                $result[now()->subDays($i)->toDateString()] = 0;
+            }
+        }
+        ksort($result);
+        $data = [
+            'statistics' => [
+                'members' => User::count(),
+                'charities' => Charity::count(),
+                'sponsors' => Sponsor::count(),
+                'events' => Activity::count(),
+            ],
+            'charts' => $result,
+        ];
+        return Response::success($data);
+    }
+}
--- routes/admin.php
+++ routes/admin.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Admin\V1\ActivityController;
 use App\Http\Controllers\Admin\V1\AdminController;
 use App\Http\Controllers\Admin\V1\AuthController;
 use App\Http\Controllers\Admin\V1\CharityController;
+use App\Http\Controllers\Admin\V1\HomeController;
 use App\Http\Controllers\Admin\V1\NewsController;
 use App\Http\Controllers\Admin\V1\PermissionController;
 use App\Http\Controllers\Admin\V1\RoleController;
@@ -25,6 +26,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('/auth/login', [AuthController::class, 'login']);
 
 Route::middleware(['auth:admin', 'scopes:place-admin', 'admin'])->group(function () {
+    Route::get('/dashboard', [HomeController::class, 'dashboard']);
     Route::post('/auth/logout', [AuthController::class, 'logout']);
     Route::apiResources([
         'admins' => AdminController::class,
