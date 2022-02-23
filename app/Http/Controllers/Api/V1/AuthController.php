880ccbf55343a89e7b26d9ca0f598e319a647a19
Zhang Hua <zhanghua_hui@qq.com>
2022-02-23 23:51:14 +0800

    fix
    Signed-off-by: Zhang Hua <zhanghua_hui@qq.com>

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -35,6 +35,7 @@ class AuthController extends Controller
         $key = 'email:register:code:' . $request->get('email');
         abort_if($request->get('code') != Cache::get($key), '422', "Verification code error");
         $user = User::create($request->all());
+        $user->update(['email_verified_at' => now()->tz(config('app.timezone'))]);
         ProcessRegOliView::dispatch($request->all());
         return Response::success($this->getLoginInfo($user));
     }
