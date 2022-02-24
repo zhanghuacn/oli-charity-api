dc53ee6072bcf8b3044dd2ff314d23527d917d3c
Zhang Hua <zhanghua_hui@qq.com>
2022-02-24 11:15:57 +0800

    fix
    Signed-off-by: Zhang Hua <zhanghua_hui@qq.com>

--- resources/views/mail/SendEmailCode.blade.php
+++ resources/views/mail/SendEmailCode.blade.php
@@ -3,27 +3,66 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <style>
-        #app {
-            line-height: 150%;
-        }
+    <base target="_blank" />
+    <style type="text/css">::-webkit-scrollbar{ display: none; }</style>
+    <style type="text/css">
+        body{font-size:14px;font-family:arial,verdana,sans-serif;line-height:1.666;padding:0;margin:0;overflow:auto;white-space:normal;word-wrap:break-word;min-height:100px}
+        td, input, button, select, body{font-family:Helvetica, 'Microsoft Yahei', verdana}
+        pre {white-space:pre-wrap;white-space:-moz-pre-wrap;white-space:-pre-wrap;white-space:-o-pre-wrap;word-wrap:break-word;width:95%}
+        th,td{font-family:arial,verdana,sans-serif;line-height:1.666}
+        img{ border:0}
+        header,footer,section,aside,article,nav,hgroup,figure,figcaption{display:block}
     </style>
 </head>
-<body>
-<div id="app">
-    <div>Dear User：</div>
-    <br/>
-    <div>Hello！</div>
-    <br/>
-    <div>Thank you for using Imagine 2080. You are in the process of {{ $operation  }}. Please enter the verification
-        code <span style="color: red">{{ $code }}</span>（valid within 15 minutes）in the verification code input box to
-        complete the verification。
-    </div>
-    <div>If it is not your own operation, please ignore this email. Please understand the inconvenience caused to you!
-    </div>
-    <br/>
-    <div>Imagine 2080</div>
-</div>
+<body tabindex="0" role="listitem">
+<table width="700" border="0" align="center" cellspacing="0" style="width:700px;">
+    <tbody>
+    <tr>
+        <td>
+            <div style="width:700px;margin:0 auto;border-bottom:1px solid #ccc;margin-bottom:30px;">
+                <table border="0" cellpadding="0" cellspacing="0" width="700" height="39" style="font:12px Tahoma, Arial, 宋体;">
+                    <tbody><tr><td width="210"></td></tr></tbody>
+                </table>
+            </div>
+            <div style="width:680px;padding:0 10px;margin:0 auto;">
+                <div style="line-height:1.5;font-size:14px;margin-bottom:25px;color:#4d4d4d;">
+                    <strong style="display:block;margin-bottom:15px;">Dear User：<span style="color:#f60;font-size: 16px;"></span></strong>
+                    <strong style="display:block;margin-bottom:15px;">
+                        Thank you for using Imagine 2080. You are in the process of
+                        <span style="font-size:16px;">
+                            {{ $operation  }}
+                        </span> .
+
+                        Please enter the verification code
+                        <span style="color: red;font-size:16px;">
+                            {{ $code }}
+                        </span>
+                        （valid within 15 minutes）in the verification code input box to
+                        complete the verification.
+                    </strong>
+                </div>
+                <div style="margin-bottom:30px;">
+                    <small style="display:block;margin-bottom:20px;font-size:12px;">
+                        <p style="color:#747474;">
+                            If it is not your own operation, please ignore this email. Please understand the inconvenience caused to you!
+                            <br>（The staff will not ask you for this verification code, please do not leak it!)
+                        </p>
+                    </small>
+                </div>
+            </div>
+            <div style="width:700px;margin:0 auto;">
+                <div style="padding:10px 10px 0;border-top:1px solid #ccc;color:#747474;margin-bottom:20px;line-height:1.3em;font-size:12px;">
+                    <p>
+                        This is a system email, please do not reply
+                        <br>
+                        Please keep your email address to avoid the account being stolen by others
+                    </p>
+                    <p>Imagine 2080</p>
+                </div>
+            </div>
+        </td>
+    </tr>
+    </tbody>
+</table>
 </body>
 </html>
