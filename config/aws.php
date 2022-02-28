325fd002c958745017495cccdc863602129bf39e
Zhang Hua <zhanghua_hui@qq.com>
2022-02-28 16:36:57 +0800

    fix
    Signed-off-by: Zhang Hua <zhanghua_hui@qq.com>

--- config/aws.php
+++ config/aws.php
@@ -17,8 +17,8 @@ return [
     |
     */
     'credentials' => [
-        'key'    => env('AWS_ACCESS_KEY_ID', 'AKIAXYCEOSTHODUBKHMQ'),
-        'secret' => env('AWS_SECRET_ACCESS_KEY', 'rKPZSPkD8katbhZ7DTPHrCCgWJYoM24tYygVk0I'),
+        'key'    => env('NULL_AWS_ACCESS_KEY_ID', ''),
+        'secret' => env('NULL_AWS_SECRET_ACCESS_KEY', ''),
     ],
     'region' => env('AWS_REGION', 'ap-southeast-2'),
     'version' => 'latest',
