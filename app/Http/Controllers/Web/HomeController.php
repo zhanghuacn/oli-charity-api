79c62bd82a9d3cee28f9f2b7d570dbd113ac90f1
Zhang Hua <zhanghua_hui@qq.com>
2022-03-08 16:35:27 +0800

    fix
    Signed-off-by: Zhang Hua <zhanghua_hui@qq.com>

--- app/Http/Controllers/Web/HomeController.php
+++ app/Http/Controllers/Web/HomeController.php
@@ -2,14 +2,10 @@
 
 namespace App\Http\Controllers\Web;
 
-use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Resources\Json\JsonResource;
-use Jiannei\Response\Laravel\Support\Facades\Response;
-
 class HomeController
 {
-    public function index(): JsonResponse|JsonResource
+    public function index()
     {
-        return Response::noContent();
+        return phpinfo();
     }
 }
---
+++ test.Dockerfile
@@ -0,0 +1,5 @@
+FROM laravelphp/vapor:php81
+
+
+
+COPY . /var/task
