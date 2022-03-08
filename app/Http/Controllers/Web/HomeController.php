d0bcc5c8e1a852fb0b1b9cdc0188a0898b367520
Zhang Hua <zhanghua_hui@qq.com>
2022-03-08 16:53:45 +0800

    fix
    Signed-off-by: Zhang Hua <zhanghua_hui@qq.com>

--- app/Http/Controllers/Web/HomeController.php
+++ app/Http/Controllers/Web/HomeController.php
@@ -2,10 +2,14 @@
 
 namespace App\Http\Controllers\Web;
 
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Jiannei\Response\Laravel\Support\Facades\Response;
+
 class HomeController
 {
-    public function index()
+    public function index(): JsonResponse|JsonResource
     {
-        phpinfo();
+        return Response::noContent();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [HomeController::class, 'index']);
+
+Route::get('/phpinfo', function () {
+    echo phpinfo();
+});
