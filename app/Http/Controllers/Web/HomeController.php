2a6845008cabfae0faab39e1ce4198094080d161
Zhang Hua <zhanghua_hui@qq.com>
2022-03-08 19:42:53 +0800

    fix
    Signed-off-by: Zhang Hua <zhanghua_hui@qq.com>

--- app/Http/Controllers/Web/HomeController.php
+++ app/Http/Controllers/Web/HomeController.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers\Web;
 
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Jiannei\Response\Laravel\Support\Facades\Response;
 
 class HomeController
 {
-    public function index(): JsonResponse|JsonResource
+    public function index(Request $request): JsonResponse|JsonResource
     {
-        return Response::noContent();
+        return Response::success($request->header('x-vapor-source-ip'));
     }
 }
