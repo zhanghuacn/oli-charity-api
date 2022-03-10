45414513b0a63c092e8ee708f59f1267accaafa0
Zhang Hua <zhanghua_hui@qq.com>
2022-03-10 15:07:36 +0800

    fix
    Signed-off-by: Zhang Hua <zhanghua_hui@qq.com>

--- app/Http/Controllers/Api/V1/Auth/LoginController.php
+++ app/Http/Controllers/Api/V1/Auth/LoginController.php
@@ -25,6 +25,9 @@ class LoginController extends Controller
             'password' => 'required|string',
         ]);
         $account = $request->get('account');
+        if (Str::substr($account, 0, 2) != '61') {
+            $account = sprintf('61%s', $account);
+        }
         $user = User::where('phone', $account)->orWhere('email', $account)->orWhere('username', $account)->first();
         if (!$user || !Hash::check($request->input('password'), $user->password)) {
             abort(422, 'The provided credentials are incorrect.');
