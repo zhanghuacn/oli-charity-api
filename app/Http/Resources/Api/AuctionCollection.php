aeefdcbf53ee000371bdb9ce3821726c42ac4c15
zhanghua <zhangkaneki@gmail.com>
2022-05-12 15:32:32 +0800

    Fix
    Signed-off-by: zhanghua <zhangkaneki@gmail.com>

--- app/Http/Resources/Api/AuctionCollection.php
+++ app/Http/Resources/Api/AuctionCollection.php
@@ -33,7 +33,7 @@ class AuctionCollection extends ResourceCollection
                 'price' => $item->price,
                 'start_time' => $item->start_time,
                 'end_time' => $item->end_time,
-                'visits' => visits($item)->count(),
+                'visits' => $item->visits()->count(),
                 'sponsor' => [
                     'id' => optional($item->auctionable)->id,
                     'name' => optional($item->auctionable)->name,
