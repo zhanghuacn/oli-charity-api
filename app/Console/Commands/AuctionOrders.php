8ad2bf88edd8fb373812f5d55992dc07890a9ab1
zhanghua <zhangkaneki@gmail.com>
2022-05-13 14:04:53 +0800

    Fix
    Signed-off-by: zhanghua <zhangkaneki@gmail.com>

--- app/Console/Commands/AuctionOrders.php
+++ app/Console/Commands/AuctionOrders.php
@@ -3,7 +3,6 @@
 namespace App\Console\Commands;
 
 use App\Mail\AuctionOrderCreated;
-use App\Mail\CaptchaShipped;
 use App\Models\Auction;
 use App\Models\Order;
 use App\Models\User;
@@ -20,10 +19,15 @@ class AuctionOrders extends Command
 
     protected $description = '生成竞拍订单';
 
-    /**
-     * @return void
-     * @throws \Throwable
-     */
+    private SnsClient $snsClient;
+
+    public function __construct(SnsClient $snsClient)
+    {
+        parent::__construct();
+        $this->snsClient = $snsClient;
+    }
+
+
     public function handle()
     {
         Auction::where([['is_auction', '=', true], ['end_time', '<', now()]])->get()
@@ -42,8 +46,29 @@ class AuctionOrders extends Command
                         $order->orderable()->associate($auction);
                         $order->save();
                         $user = User::findOrFail($auction->current_bid_user_id);
-                        if (!empty($user)) {
+                        if (!empty($user->email)) {
                             Mail::to($user->email)->send(new AuctionOrderCreated($auction));
+                        }
+                        if (!empty($user->phone)) {
+                            $bid_num = $auction->bidRecord()->where(['user_id' => $auction->current_bid_user_id])->count();
+                            $user_count = $auction->bidRecord()->groupBy('user_id')->count();
+
+                            $this->snsClient->publish([
+                                'Message' => sprintf(
+                                    "【%s】Congratulations! You've won the auction with an AU %s. Next, please make a payment to receive your item. You placed %s bids and beat %s bidders.",
+                                    config('app.name'),
+                                    $order->amount,
+                                    $bid_num,
+                                    $user_count
+                                ),
+                                'PhoneNumber' => sprintf('+%s', $user->phone),
+                                'MessageAttributes' => [
+                                    'AWS.SNS.SMS.SMSType' => [
+                                        'DataType' => 'String',
+                                        'StringValue' => 'Transactional',
+                                    ]
+                                ],
+                            ]);
                         }
                     }
                     $auction->is_auction = false;
