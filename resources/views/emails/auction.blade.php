ee61d4e28e92edcd08eca633d33b62460564265a
zhanghua <zhangkaneki@gmail.com>
2022-05-20 20:25:34 +0800

    Fix
    Signed-off-by: zhanghua <zhangkaneki@gmail.com>

--- resources/views/emails/auction.blade.php
+++ resources/views/emails/auction.blade.php
@@ -2,11 +2,11 @@
 ### <center><img src="https://s3.imagine2080.com.au/public/email/imagine2080Logo.svg" alt="{{ config('app.name') }}"></center>
 # Dear {{ $username }},
 
-<p>Congratulations! You've won the auctio {{ $auction->name }} with an AU ${{ $auction->current_bid_price }}. Next, please make a payment to receive your item.</p>
+<p>Congratulations! You've won the auction {{ $auction->name }} with an AU ${{ $auction->current_bid_price }}. Next, please make a payment to receive your item.</p>
 <p>You placed {{ $bid_num }} bids and beat {{ $user_num }} bidders.</p>
 <p>If you have any questions, please contact the administrator of the WeChat group.</p>
 
-<center><img src="{{ $image }}" alt="{{ $auction->name }}" width="300" height="300"/></center>
+<center><img src="{{ $image }}" alt="{{ $auction->name }}" width="500" height="300"/></center>
 
 Thanks,<br>
 {{ config('app.name') }} Teams
